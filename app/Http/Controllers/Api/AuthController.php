96f997082391800a430cd0b86e7cc84245bf0133
golib0700 <mbozorboyev571@gmail.com>
2026-05-16 17:54:10 +0500

    commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login (Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if (!Auth::attempt($credentials)){
+            return response()->json([
+                'message' => 'Foydalanuvchi_Id yoki Parol noto`g`ri'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        if (!$user->is_active){
+            return response()->json([
+                'message' => 'Hisob bloklangan'
+            ], 403);
+        }
+
+        $token = $user->createToken('crm-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Muvaffaqiyatli kirish.',
+            'token' => $token,
+            'user' => $user
+        ]);
+
+    }
+    //
+}
